817fb35190ce88de96c9f3746140b8c86de9a67e
shrvya <shrivyashetty@gmail.com>
2021-08-28 19:19:14 +0530

    added:welcome

---
+++ Maximum.java
@@ -0,0 +1,15 @@
+package generics;
+
+/*
+ * this class will will find maximum of 3 numbers
+ * it has a method maximum to find maximum
+ * it has 3 testcases
+ */
+public class Maximum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+
+	}
+}
